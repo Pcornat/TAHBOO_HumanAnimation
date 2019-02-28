79373879403d7c013b0781c9221aa19afac0ca6b
Florent Denef <florent.denef@gmail.com>
2019-02-28 11:54:54 +0100

    windows partially integrated.

---
+++ for_windows.h
@@ -0,0 +1,11 @@
+#ifndef HUMANANIMATION_FOR_WINDOWS_H
+#define HUMANANIMATION_FOR_WINDOWS_H
+
+#ifdef _WIN32
+
+#include <windows.h>
+#include "stdafx.h"
+
+#endif
+
+#endif //HUMANANIMATION_FOR_WINDOWS_H
